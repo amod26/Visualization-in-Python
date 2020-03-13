3179011d665943bbb2fff4f73ccd6de91e0ea872
Amod Panchal <32383876+amod26@users.noreply.github.com>
2020-03-13 13:53:53 -0400

    Turtle Square
    Drawing a square using Turtle function.
    
    #import turtle

---
+++ Turtle Square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+
+
+# In[2]:
+
+
+raphael = turtle.Turtle()
+
+
+# In[4]:
+
+
+raphael.left(90) # looks left 90 degree
+
+
+# In[8]:
+
+
+raphael.forward(100) # move forward 100 steps
+raphael.right(90) # looks right 90 degree
+raphael.forward(100)
+raphael.right(90)
+raphael.forward(100)
+raphael.right(90)
+raphael.forward(100) 
+
+
+# In[11]:
+
+
+raphael.circle(360) #draws a 360 degree circle
+raphael.circle(180) #draws a 180 degree circle
+
+
+# In[17]:
+
+
+turtle.bye() # shuts the graphic window
+
+
+# In[ ]:
+
+
+
+
