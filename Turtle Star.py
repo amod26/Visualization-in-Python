78f8698c0610f3ba23882f38bd2acce028d9b229
Amod Panchal <32383876+amod26@users.noreply.github.com>
2020-03-13 14:16:59 -0400

    Turtle Star
    Created a star using Turtle function and filled it with color.

---
+++ Turtle Star.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import turtle
+
+
+# In[14]:
+
+
+donatelo = turtle.Turtle()
+
+
+# In[27]:
+
+
+donatelo.fillcolor('#FFA500')
+donatelo.begin_fill()
+for i in range(5):
+    donatelo.forward(150)
+    donatelo.right(144)
+donatelo.end_fill()
+
+
+# In[26]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
