54828e1a078996fc25a1e34012a885a3edc85b66
Amod Panchal <32383876+amod26@users.noreply.github.com>
2020-03-13 14:05:29 -0400

    Turtle Star
    Creating a star using the turtle function.

---
+++ Turtle Star.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import turtle
+
+
+# In[14]:
+
+
+donatelo = turtle.Turtle()
+
+
+# In[23]:
+
+
+for i in range(5):
+    donatelo.forward(150)
+    donatelo.right(144)
+
+
+# In[16]:
+
+
+
+
